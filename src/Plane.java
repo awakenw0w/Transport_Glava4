6b685f057c947660c5e29276a17655a6eac09585
awakenw0w <awakenwow42@gmail.com>
2025-02-16 15:59:55 +0300

    Create Plane.java

---
+++ src/Plane.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Plane extends Transport {
+    private int flyingHeight; //высота полета
+
+    //конструктор
+    public Plane(int power, int cost, String number, String brand, int flyingHeight){
+        super(power, cost, number, brand);
+        this.flyingHeight = flyingHeight;
+    }
+
+    public int getFlyingHeight(){
+        return flyingHeight;
+    }
+
+}
